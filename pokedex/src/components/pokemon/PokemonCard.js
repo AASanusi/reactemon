cf472aaf3e991b0487e79f07dcdbd1b3f64094d0
AASanusi <alaminm.sanusi@gmail.com>
2024-04-18 23:30:36 +0100

    Add pokemon from API and request using axios

---
+++ pokedex/src/components/pokemon/PokemonCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class PokemonCard extends Component {
+  render() {
+    return (
+      <div className="col-md-3 col-sm-6 mb-5">
+        <div class="card">
+          <div class="card-header">
+            <h1>AASanusi</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}

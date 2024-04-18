c21b87dd154354489b6b65399c6e21bf9b498f67
AASanusi <alaminm.sanusi@gmail.com>
2024-04-19 00:17:27 +0100

    Style cards

--- pokedex/src/components/pokemon/PokemonCard.js
+++ pokedex/src/components/pokemon/PokemonCard.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 
 export default class PokemonCard extends Component {
+  state = {
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
+  };
+
+  componentDidMount() {
+    const { name, url } = this.props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    const imageURL = `https://github.com/PokeAPO/sprits/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
+    this.setState({
+      name,
+      imageURL,
+      pokemonIndex,
+    });
+  }
+
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
-        <div class="card">
-          <div class="card-header">
-            <h1>AASanusi</h1>
+        <div className="card">
+          <h5 className="card-header">{this.state.pokemonIndex}</h5>
+          <div className="card-body mx-auto">
+            <h6 className="card-title">{this.state.name}</h6>
           </div>
         </div>
       </div>
